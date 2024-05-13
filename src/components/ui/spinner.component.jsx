8409c8faf6fb6f8f0bc4d9ae87443eea02bf2db0
Yeganeh-Hossein <138772245+Yeganeh-Hossein@users.noreply.github.com>
2024-05-13 21:18:12 +0430

    Create spinnerComponents

---
+++ src/components/ui/spinner.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import SpinnerImg from '../../assets/images/spinner.gif';
+const Spinner = () =>  {
+        return (
+            <div className='d-flex justify-content-center align-items-center'>
+                <img src={SpinnerImg}     alt="spinner"  />
+            </div>
+        );
+};
+export default Spinner;
--- src/pages/Home.pages.jsx
+++ src/pages/Home.pages.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import Spinner from '../components/ui/spinner.component';
 const Homepage =() => {
     return (
         <div>
             <h5>Home page</h5>
             <h5>Home page</h5>
+            <Spinner/>
             <h5>Home page</h5>
             <h5>Home page</h5>
          
