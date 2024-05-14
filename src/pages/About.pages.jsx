2a62dedd863af92bad9e50def92056d6c28d28fa
Yeganeh-Hossein <138772245+Yeganeh-Hossein@users.noreply.github.com>
2024-05-14 16:26:54 +0430

    create a contact and about us page

--- src/pages/About.pages.jsx
+++ src/pages/About.pages.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
-
-const about =() => {
-    return (
-        <div>
-            <h1>about</h1>
-            <h1>about</h1>
-            <h1>about</h1>
+import React from "react";
+import AboutImg from "../assets/images/about.png";
+const About = () => {
+  return (
+    <div className="container">
+      <div className="row col-12 bg-info p-3 rounded">
+        <h3>About us</h3>
+      </div>
+      <div className="row align-items-center">
+        <div className="col-md-7 ">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus
+          sint quibusdam itaque non facere et, perspiciatis earum vel impedit
+          fugit! Voluptates, vel dolor culpa ea reiciendis et! Omnis placeat
+          commodi id blanditiis praesentium reiciendis maxime aliquid veniam
+          quas quidem suscipit itaque quasi debitis, recusandae vel iure eum non
+          cum expedita quo maiores, modi quos esse. Facere eveniet qui cumque.
+          Distinctio voluptatem laborum sit fugiat cupiditate expedita vero sed
+          explicabo ad. Eaque, consectetur ut impedit cumque perferendis
+          dolorem. Ipsa impedit exercitationem ducimus similique reprehenderit
+          iusto repellendus nostrum, earum numquam totam, debitis aspernatur vel
+          itaque sint et delectus ab sed quasi asperiores?
         </div>
-    );
+        <div className="col-md-5 rounded text-center">
+          <img src={AboutImg} alt="AboutImg" className="img-fluid AboutImg" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default about;
+export default About;
--- src/pages/Contact.pages.jsx
+++ src/pages/Contact.pages.jsx
@@ -1,14 +1,57 @@
-import React from 'react'
-
-const  contact = () => {
-    return (
-        <div>
-            <h5>contact</h5>
-            <h5>contact</h5>
-            <h5>contact</h5>
-            <h5>contact</h5>
+import React from "react";
+import contactImg from "../assets/images/contact.png";
+const Contact = () => {
+  return (
+    <div className="container">
+      <div className="row">
+      <div className="col-12 bg-info p-3 rounded">
+        <h3>Contact us</h3>
+      </div>
+      </div>
+      <div className="row align-items-center">
+        <div className="col-md-7 ">
+          <div className="mt-3">
+            <h3 className="text-primary">How to contact us :</h3>
+            <h5>+98 911 369 7249</h5>
+            <h5>hosseinyeganeh2023@gmail.com</h5>
+          </div>
+          <div className="mt-3">
+    
+              <div className="mb-3">
+                <label className="form-label">
+                   Your Name:
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="please type your name:"
+                />
+                
+              </div>
+              <div className="mb-3">
+                <label className="form-label">
+                   Your Message:
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="please type your Message:"
+                />
+                
+              </div>
+              <button className="btn btn-outline-primary ms-3">send</button>
+          </div>
         </div>
-    )
-}
+        <div className="col-md-5 rounded text-center">
+          <img
+            src={contactImg}
+            alt="contactImg"
+            className="img-fluid contactImg"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default contact;
+export default Contact;
