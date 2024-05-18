958ae10046846ea04dd072ada6af517bdd7c51c8
Yeganeh-Hossein <138772245+Yeganeh-Hossein@users.noreply.github.com>
2024-05-18 18:03:16 +0430

    create Homepage

--- src/pages/Home.pages.jsx
+++ src/pages/Home.pages.jsx
@@ -1,17 +1,43 @@
-import React from 'react'
-import Spinner from '../components/ui/spinner.component';
-const Homepage =() => {
-    return (
-        <div>
-            <h5>Home page</h5>
-            <h5>Home page</h5>
-            <Spinner/>
-            <h5>Home page</h5>
-            <h5>Home page</h5>
-         
-            
+import React, { useEffect, useState } from "react";
+import Spinner from "../components/ui/spinner.component";
+import axios from "axios";
+import { baseUrl } from "../constants/api";
+
+const Homepage = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    setLoading(true);
+    axios(baseUrl)
+      .then((response) => {
+        setData(response.data);
+        setLoading(false);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        setError("An error occurred");
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12 text-center">
+          <h1>Crypto Tracking</h1>
+          <h4>A place to manage all of your trade informations and actions</h4>
+          <h4>Do not forget to always using assets and risk management!!!</h4>
         </div>
-    );
+      </div>
+      <div className="row">
+        <div className="col-12">
+          {loading ? <Spinner /> : error !== "" ? <h1>{error}</h1> : <h1>Data is ready</h1>}
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default Homepage ;
+export default Homepage;
